d0b5ce128a2b40f9dbb306756f3f5ee1bc217d38
sassofrancesco00 <47978951+sassofrancesco00@users.noreply.github.com>
2025-11-25 12:25:41 +0100

    Add frontend and backend full structure

---
+++ backend/src/main/java/com/awesomepizza/order_management/AwesomePizzaOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.awesomepizza.order_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomePizzaOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomePizzaOrderManagementApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/awesomepizza/order_management/AwesomePizzaOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awesomepizza.order_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomePizzaOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
